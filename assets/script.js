0f8ed82c2ff903916dc2ec21017dd3d645776edc
kamal1198 <kaur.preetkamal1198@gmail.com>
2022-05-18 16:58:16 -0400

    added function for click

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,10 @@
+//date,day
 var todaydate = moment().format("mm-do-yyyy");
 $("#currentday").html(todaydate);
+//add click function
+$(document).ready(function) {
+$(".savebtn").on ("click",function()){
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+}
+}
